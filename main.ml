f872857d6ae4ae1fe99a4bbdd5be7cad7de766af
Callum Rogers <callumrogers@hotmail.co.uk>
2012-03-13 17:12:25 +0000

    Added printing of parser output.

--- main.ml
+++ main.ml
@@ -1,15 +1,32 @@
-open Lexer
-open Lexing
 open Print
 open Printf
+open Lexing
+
+let print_lex file =
+	let in_chan = open_in file in
+	let lexbuf = Lexing.from_channel in_chan in
+	while not lexbuf.lex_eof_reached do
+		Printf.printf "%s " (fmt_token (Lexer.token lexbuf))
+	done
+
+let print_parse prog = print_endline (fmt_program prog)
 
 let main () =
 	let args = Sys.argv in
 	let file = Array.get args 1 in
 	let in_chan = open_in file in
 	let lexbuf = Lexing.from_channel in_chan in
-	while not lexbuf.lex_eof_reached do
-		Printf.printf "%s " (fmt_token (Lexer.token lexbuf))
-	done
+	let prog = try Parser.program Lexer.token lexbuf with
+		| Parsing.Parse_error -> 
+			let token = Lexing.lexeme lexbuf in
+			err_msg ("Parser could not recognise token " ^ token) !Lexer.lineno file;
+			Printf.printf "\nLexer output:\n";
+			print_lex file;
+			Tree.Program [] in
+	
+	print_parse prog;
+	
+	close_in in_chan;
+	exit 0
 
 let funell = main ()
--- print.ml
+++ print.ml
@@ -1,5 +1,6 @@
 open Parser
 open Lexer
+open Tree
 
 let revtbl ht n =
 	let rev = Hashtbl.create n in
@@ -7,6 +8,17 @@ let revtbl ht n =
 	rev
 
 let backwardtable = revtbl kwtable 64
+
+let read_lineno file lineno =
+	let in_chan = open_in file in
+	Printf.printf "file: %s, line: %d" file lineno;
+	for i=1 to lineno-1 do
+		input_line in_chan
+	done;
+	input_line in_chan
+
+let err_msg msg lineno file =
+	Printf.printf "Error: %s at line %d in file %s:\n%s\n" msg lineno file (read_lineno file lineno)
 
 let fmt_token =
 	function
@@ -24,4 +36,20 @@ let fmt_token =
 		| IN -> "IN"
 		| GUARD -> "GUARD"
 		| TYPEDECL -> "TYPEDECL"
-		| BADTOK -> "BADTOK"
+		| BADTOK s -> "(BADTOK " ^ s ^ ")"
+
+let id s = s
+
+let fmt_list formatter list = "[" ^ (List.fold_left (fun result item -> result ^ formatter item ^ "; ") "" list) ^ "]"
+
+let rec fmt_expr =
+	function
+		| Call (name, args) -> Printf.sprintf "(Call %s %s)" name (fmt_list fmt_expr args)
+
+let rec fmt_decl =
+	function
+		| Decl (name, args, e) -> Printf.sprintf "(Decl %s %s %s)" name (fmt_list id args) (fmt_expr e)
+
+let rec fmt_program = 
+	function
+		| Program decls -> List.fold_left (fun result decl -> result ^ "\n" ^ fmt_decl decl) "" decls
