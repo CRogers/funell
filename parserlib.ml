eeea1ac1b81cf50a31997875c0911fd12a025a6c
Callum Rogers <callumrogers@hotmail.co.uk>
2012-03-26 00:07:02 +0100

    Added exception when trying to get an non-existent operator

--- parserlib.ml
+++ parserlib.ml
@@ -1,3 +1,5 @@
+exception Operator_not_found of string
+
 open Parser
 
 (* A hashtable to keep a list of infix operator precedents *)
@@ -6,4 +8,7 @@ let optable: (string, Parser.token) Hashtbl.t =
 	List.iter (fun (k,v) -> Hashtbl.add ht k v) [("+", OPL2 "+"); ("-", OPL2 "-"); ("*", OPL3 "*"); ("/", OPL4 "/")];
 	ht 
 
-let getOperator op = Hashtbl.find optable op 
+let getOperator op = 
+	try Hashtbl.find optable op
+	with Not_found -> raise (Operator_not_found op)
+ 
